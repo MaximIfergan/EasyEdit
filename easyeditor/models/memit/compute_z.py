1077c02fe727a4986ab33891abb2182a2cd2a979
maxim <maxim758@gmail.com>
2024-03-12 13:31:31 +0200

    Try to run Memit on 4 L4 GPU

--- easyeditor/models/memit/compute_z.py
+++ easyeditor/models/memit/compute_z.py
@@ -100,6 +100,8 @@ def compute_z(
                 # Initial value is recorded for the clean sentence
                 target_init = cur_out[0][0, lookup_idxs[0]].detach().clone()
 
+            cur_out.to(f"cuda:{hparams.device}")
+
             # Add intervened delta
             for i, idx in enumerate(lookup_idxs):
 
