5a937916547e7df4c95ead1b5cf9695eb538a4fb
maxim <maxim758@gmail.com>
2024-03-13 19:04:06 +0200

    Try to run Memit on 4 L4 GPU

--- easyeditor/models/memit/compute_z.py
+++ easyeditor/models/memit/compute_z.py
@@ -110,9 +110,9 @@ def compute_z(
                     print(cur_out[0][i, idx, :].get_device())
                     print("delta.get_device()")
                     print(delta.get_device())
-                    delta.to(f"cuda:{cur_out[0][i, idx, :].get_device()}")
-                    print("delta.get_device()")
-                    print(delta.get_device())
+                    cur_out[0].to(f"cuda:{delta.get_device()}")
+                    print("cur_out[0][i, idx, :].get_device()")
+                    print(cur_out[0][i, idx, :].get_device())
                     cur_out[0][i, idx, :] += delta
 
         return cur_out
