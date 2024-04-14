18e20382d891a13640fa05c82c28cc3dbe8535dc
maxim <maxim758@gmail.com>
2024-04-14 18:32:45 +0300

    Scaling Dataset

--- easyeditor/models/ft/ft_main.py
+++ easyeditor/models/ft/ft_main.py
@@ -192,20 +192,6 @@ def execute_ft(
                 loss_fct = CrossEntropyLoss(ignore_index=-100)
                 loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
                 loss = loss.to(lm_logits.dtype)
-            elif 'bloom' in hparams.model_name.lower():
-                if hparams.objective_optimization == 'prompt_last':
-                    logits = model(**inputs).logits
-                    probs = torch.nn.functional.log_softmax(logits[torch.arange(bs), last_token_inds], dim=-1)
-                    loss = -(torch.gather(probs, 1, target_ids) * loss_mask).sum(1) / loss_mask.sum(1)
-                elif hparams.objective_optimization == 'target_new':
-                    logits = model(**inputs_targets).logits
-                    shift_logits = logits[..., :-1, :].contiguous()
-                    shift_labels = inputs_targets['input_ids'][..., 1:].contiguous()
-                    loss_fct = CrossEntropyLoss(reduction='none')
-                    loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
-                    loss = loss.view(bs, -1)
-                    loss = (loss * label_mask[:, 1:]).sum(1) / label_mask[:, 1:].sum(1)
-                    loss = loss.mean()
             else:
                 if hparams.objective_optimization == 'prompt_last':
                     probs = torch.nn.functional.log_softmax(
@@ -230,9 +216,6 @@ def execute_ft(
 
             if loss.item() >= 1e-2:
                 loss.backward()
-
-                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-
                 opt.step()
             else:
                 print("Dont update")
